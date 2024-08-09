af1ecfbffaeb9df2d32c4136f148e32846748584
funa ryo <ryo@mac2.local>
2024-08-09 22:16:38 +0900

    feat: crystal done

--- program/Crystal/Crystal.cr
+++ program/Crystal/Crystal.cr
@@ -1,7 +1,14 @@
-n, k = read_line.split.map(&.to_i64)
-a = read_line.split.map(&.to_i64)
-a.each_with_index do |x, i|
-  if x % k == 0
-    puts (x / k).to_i
-  end
+#Crystal (Crystal 1.9.1)
+n = (1).to_i
+while n <= 100
+    if n % 15 == 0
+        puts "FizzBuzz"
+    elsif n % 3 == 0
+        puts "Fizz"
+    elsif n % 5 == 0
+        puts "Buzz"
+    else
+        puts n
+    end
+    n += 1
 end
